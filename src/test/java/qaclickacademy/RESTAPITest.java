2e43e71a736379691a058865f88a74fce75395de
chandan-hn2023 <chandanhngowda@gmail.com>
2023-05-12 00:09:42 +0530

    EST time zone

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -16,7 +16,7 @@ public class RESTAPITest {
 		System.out.println("postJira4");
 		System.out.println("postJira5");
 		System.out.println("postJira6");
-		System.out.println("postJira7");
+		System.out.println("postJira777");
 		
 		
 		
